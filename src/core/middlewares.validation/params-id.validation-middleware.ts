3848ef05c4cba58fb4413adf9fdafaffa2a56389
Stanislav Sydorenko <goneinterlude@gmail.com>
2026-03-18 09:33:06 +0200

    changes

--- src/core/middlewares.validation/params-id.validation-middleware.ts
+++ src/core/middlewares.validation/params-id.validation-middleware.ts
@@ -7,5 +7,4 @@ export const idValidation = param('id')
     .withMessage('ID must be a string') // Проверка, что это строка
     .isLength({ min: 1 })
     .withMessage('ID must not be empty') // Проверка, что строка не пустая
-    .isNumeric()
     .withMessage('ID must be a numeric string');
